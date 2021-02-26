1d40b79222208d50630508aa6840ace1e05014be
cyberfather <79603958+cyberfather@users.noreply.github.com>
2021-02-26 10:58:53 -0800

    Create passlist.py

---
+++ passlist.py
@@ -0,0 +1,9 @@
+#cyberfatherr
+
+ls = [0,1,2,3,4,5,6,7,8,9]
+
+for i in range(0,10):
+    for j in range(0,10):
+        for k in range(0,10):
+            for l in range(0,10):
+                print(ls[i],ls[j],ls[k],ls[l])
